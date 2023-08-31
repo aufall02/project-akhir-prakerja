6e9ea9858c910b2c07682b662c4c42852c02653a
aufall02 <aufaluzumaki@gmail.com>
2023-08-31 19:38:25 +0700

    init database

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	InitDatabase()
+}
+
+type Config struct {
+	username string
+	password string
+	port     string
+	host     string
+	name     string
+}
+
+func InitDatabase() {
+	config := Config{
+		username: os.Getenv("DB_USERNAME"),
+		password: os.Getenv("DB_PASSWORD"),
+		port:     os.Getenv("DB_PORT"),
+		host:     os.Getenv("DB_HOST"),
+		name:     os.Getenv("DB_NAME"),
+	}
+
+	conn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config.username,
+		config.password,
+		config.host,
+		config.port,
+		config.name,
+	)
+
+	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
+	DB = db
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"final-project-prakerja/config"
+	"fmt"
+)
 
 func main() {
-	
+	fmt.Println("test")
+	db :=  config.DB.Migrator().CurrentDatabase()
+	fmt.Println(db)
 }
