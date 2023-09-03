328a1a3a873402f70bca1f6eac6c10b078ac3518
aufall02 <aufaluzumaki@gmail.com>
2023-09-03 07:18:17 +0700

    set rout usercurrent

---
+++ routes/user.route.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"final-project-prakerja/controllers"
+	"final-project-prakerja/midleware"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserRouteController struct {
+	userController controllers.UserController
+}
+
+func NewRouteUserController(userController controllers.UserController) UserRouteController {
+	return UserRouteController{userController}
+}
+
+func (uc *UserRouteController) UserRoute(e echo.Group){
+	router := e.Group("/users", midleware.DeserializeUser)
+	router.GET("/usercurrent", uc.userController.GetCurrent)
+}
