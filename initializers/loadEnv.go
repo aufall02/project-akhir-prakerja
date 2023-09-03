6b47479598ac43f801512531fb6a593f241bb87e
aufall02 <aufaluzumaki@gmail.com>
2023-09-03 07:16:36 +0700

    sek environment

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"errors"
+	// "fmt"
 	"os"
 )
 
@@ -12,10 +13,21 @@ type Config struct {
 	host        string
 	name        string
 	appname     string
-	port_server string
+	Port_server string
+
+	ClientOrigin string
+
+	AccessTokenPrivateKey  string
+	AccessTokenPublicKey   string
+	RefreshTokenPrivateKey string
+	RefreshTokenPublicKey  string
+	AccessTokenExpiresIn   string
+	RefreshTokenExpiresIn  string
+	AccessTokenMaxAge      string
+	RefreshTokenMaxAge     string
 }
 
-func LoadEnv() (Config,error) {
+func LoadEnv() (Config, error) {
 	config := Config{
 		username:    os.Getenv("DB_USERNAME"),
 		password:    os.Getenv("DB_PASSWORD"),
@@ -23,13 +35,25 @@ func LoadEnv() (Config,error) {
 		host:        os.Getenv("DB_HOST"),
 		name:        os.Getenv("DB_NAME"),
 		appname:     os.Getenv("APP_NAME"),
-		port_server: os.Getenv("SERVER_PORT"),
+		Port_server: os.Getenv("SERVER_PORT"),
+
+
+		ClientOrigin: os.Getenv("CLIENT_ORIGIN"),
+
+		AccessTokenPrivateKey:  os.Getenv("ACCESS_TOKEN_PRIVATE_KEY"),
+		AccessTokenPublicKey:   os.Getenv("ACCESS_TOKEN_PUBLIC_KEY"),
+		RefreshTokenPrivateKey: os.Getenv("REFRESH_TOKEN_PRIVATE_KEY"),
+		RefreshTokenPublicKey:  os.Getenv("REFRESH_TOKEN_PUBLIC_KEY"),
+		AccessTokenExpiresIn:   os.Getenv("ACCESS_TOKEN_EXPIRED_IN"),
+		RefreshTokenExpiresIn:  os.Getenv("REFRESH_TOKEN_EXPIRED_IN"),
+		AccessTokenMaxAge:      os.Getenv("ACCESS_TOKEN_MAXAGE"),
+		RefreshTokenMaxAge:     os.Getenv("REFRESH_TOKEN_MAXAGE"),
 	}
 
-	if config.name == ""  {
+	if config.name == "" {
 		return config, errors.New("host not found")
-	} 
-
+	}
+	// fmt.Println( "private key : "+config.AccessTokenPrivateKey)
 
 	return config, nil
 }
